05bb0f181e421690a3500ad8cf10c7873589d4ac
Hope Diebold <epohdespair@gmail.com>
2022-01-11 18:42:30 -0500

    updated app.js

--- app.py
+++ app.py
@@ -3,13 +3,20 @@ from flask import Flask, jsonify, request, render_template
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-app.config["MONGO_URI"]="mongodb://localhost:27017/fetal_database"
-mongo = PyMongo(app)
+# app.config["MONGO_URI"]="mongodb://localhost:27017/fetal_database"
+# mongo = PyMongo(app)
 
 # establish route
 @app.route("/")
 def home_page():
     return render_template('index.html')
+
+@app.route('/flask', methods=['GET'])
+def index():
+    return "Flask server"
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
 
 # run app
 app.run(debug=True)
--- templates/static/js/app.js
+++ templates/static/js/app.js
@@ -1,52 +1,49 @@
 // read in data
-const MongoClient = require('mongodb').MongoClient
-const url = 'mongodb://localhost:27017'
+// connect to database
+const express = require("express")
+const mongoose = require("mongoose")
+const request = require('request');
 
-// define variables
-MongoClient.connect(url, {
+const app = express();
+
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/fetal_database',
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}, (err, client) => {
-    if (err) {
-        return console.log(err);
-    }
-    // Specify database you want to access
-    const db = client.db('fetal_database');
-    console.log(`MongoDB Connected: ${url}`);
+  }
+);
+
+// check connection
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
 });
+
+// creation of server
+// app.use(Router);
+
+const PORT = 3000;
+
+app.get('/home', function(req, res) {
+    request('http://127.0.0.1:5000/flask', function (error, response, body) {
+        console.error('error:', error); // Print the error
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body); // Print the data received
+        res.send(body); //Display the response on the website
+      });      
+});
+
+app.listen(PORT, function (){ 
+    console.log('Listening on Port 3000');
+});  
 
 // state
 // race/hispanic_origin
 // age
 // delivery place
 // delivery method
+// fetal death count
 
-// heatmap state vs fetal deaths
-
-// apply layout 
-
-// deploy map 
-
-// pie chart race/hispanic origin vs fetal deaths
-
-// apply layout 
-
-// deploy map 
-
-// line graph age of mother vs # of fetal deaths
-
-// apply layout 
-
-// deploy map 
-
-// bar graph delivery place vs # of fetal deaths
-
-// apply layout 
-
-// deploy map 
-
-// lollipop deliver method vs fetal deaths
-
-// apply layout 
-
-// deploy map 
