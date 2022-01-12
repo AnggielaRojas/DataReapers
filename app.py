a1aab8f35de954f9bd5f1d63e3cda32d4e1859a7
Hope Diebold <epohdespair@gmail.com>
2022-01-11 21:47:18 -0500

    updated app.py to connect to PostGres

--- app.py
+++ app.py
@@ -1,22 +1,47 @@
 # import dependencies
+import sqlalchemy
+import numpy as np
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 from flask import Flask, jsonify, request, render_template
-from flask_pymongo import PyMongo
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql.expression import all_
 
+# establish app
 app = Flask(__name__)
-# app.config["MONGO_URI"]="mongodb://localhost:27017/fetal_database"
-# mongo = PyMongo(app)
 
-# establish route
+# establish database setup
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/fetal_db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+Base = automap_base()
+Base.prepare(db.engine, reflect = True)
+
+# reference to tables
+fetal_data = Base.classes.fetal_db
+
+# establish route home page
 @app.route("/")
 def home_page():
     return render_template('index.html')
 
-@app.route('/flask', methods=['GET'])
-def index():
-    return "Flask server"
+# # establish route for data
+@app.route("/api/fetal_data")
+def fetaldata():
+    session =Session(engine)
+    fetaldata = session.query(
+        fetal_data.state,
+        fetal_data.race_hispanic_origin,
+        fetal_data.age,
+        fetal_data.delivery_place,
+        fetal_data.delivery_method,
+        fetal_data.fetal_deaths
+    ).all()
+    session.close()
+    all_fetaldata = list(np.ravel(fetal_data))
+    return jsonify(all_fetaldata)
 
-if __name__ == "__main__":
-    app.run(port=5000, debug=True)
 
 # run app
 app.run(debug=True)
--- templates/static/js/app.js
+++ templates/static/js/app.js
@@ -1,44 +1,4 @@
 // read in data
-// connect to database
-const express = require("express")
-const mongoose = require("mongoose")
-const request = require('request');
-
-const app = express();
-
-app.use(express.json());
-
-mongoose.connect('mongodb://localhost:27017/fetal_database',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
-);
-
-// check connection
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", function () {
-  console.log("Connected successfully");
-});
-
-// creation of server
-// app.use(Router);
-
-const PORT = 3000;
-
-app.get('/home', function(req, res) {
-    request('http://127.0.0.1:5000/flask', function (error, response, body) {
-        console.error('error:', error); // Print the error
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log('body:', body); // Print the data received
-        res.send(body); //Display the response on the website
-      });      
-});
-
-app.listen(PORT, function (){ 
-    console.log('Listening on Port 3000');
-});  
 
 // state
 // race/hispanic_origin
